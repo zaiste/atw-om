7b8b0c9c3728420ee6239339b7863a52acda7f56
Zaiste! <oh@zaiste.net>
2014-12-27 00:23:35 +0100

    improv: update deps

--- src/leiningen/new/atw_om/project.clj
+++ src/leiningen/new/atw_om/project.clj
@@ -2,22 +2,23 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.4"]
-                 [ring/ring-core "1.2.0"]
-                 [ring/ring-json "0.2.0"]
-                 [compojure "1.1.6"]
-                 [org.clojure/clojurescript "0.0-2371"]
+                 [ring/ring-core "1.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [compojure "1.3.1"]
+                 [org.clojure/clojurescript "0.0-2511"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                 [cljs-http "0.1.9"]
-                 [om "0.7.3"]]
+                 [cljs-http "0.1.23"]
+                 [om "0.8.0-beta5"]
+                 [figwheel "0.2.0-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.0.3"]
-            [lein-ring "0.8.11"]
-            [lein-pdo "0.1.1"]]
+            [lein-ring "0.8.13"]
+            [lein-pdo "0.1.1"]
+            [lein-figwheel "0.2.0-SNAPSHOT"]]
 
   :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
 
-  :min-lein-version "2.0.0"
+  :min-lein-version "2.5.0"
   :uberjar-name "{{name}}-standalone.jar"
 
   :ring {:handler {{name}}.core/app
